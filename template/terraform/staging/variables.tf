6912113897fd83f028086a4ca80963cc75bd300d
Ege Kocabas <kocabas.ege@tum.de>
2024-01-08 18:13:55 +0100

    add environment variable

--- template/terraform/staging/variables.tf
+++ template/terraform/staging/variables.tf
@@ -2,3 +2,11 @@ variable "ANALYTICS_BASE_URL" {
   description = "value"
   type        = string
 }
+
+/* ------------------------ Analytics and Sourcing Auth Flow ------------------------ */
+
+variable "SHARED_SECRET_KEY" {
+  description = "Shared secret key for the analytics and sourcing auth flow"
+  type        = string
+  sensitive   = true
+}
